5e03a1d9d65ad6bbbe77d8745ac54a86b69bb299
tienshaoku <tienshaoku@gmail.com>
2023-04-14 15:23:05 +0900

    # Repo setup.

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
+import "@typechain/hardhat"
+import "hardhat-contract-sizer"
+import "hardhat-dependency-compiler"
+import { HardhatUserConfig } from "hardhat/config"
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.7.6",
+        settings: {
+            optimizer: { enabled: true, runs: 200 },
+            evmVersion: "berlin",
+            // for smock to mock contracts
+            outputSelection: {
+                "*": {
+                    "*": ["storageLayout"],
+                },
+            },
+        },
+    },
+    networks: {
+        localhost: {
+            allowUnlimitedContractSize: true,
+        },
+    },
+    contractSizer: {
+        // max bytecode size is 24.576 KB
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: true,
+        except: ["@openzeppelin/", "test/"],
+    },
+}
+
+export default config
