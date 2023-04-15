a2c84b0060a17daeb19918e0679a49e5a71cb319
42bchen <boyuchen0224@gmail.com>
2023-04-15 23:59:36 +0900

    Add register script

--- script/register.ts
+++ script/register.ts
@@ -1,22 +1,40 @@
 import { ethers, deployments } from "hardhat"
-import { PolyNftFactory } from "../typechain-types"
+import { IPolyNftErc721, PolyNftFactory, PolyNftRegistry } from "../typechain-types"
+import { parseEther } from "ethers/lib/utils"
 
 async function main() {
+    const polyNftFactoryDeployments = await deployments.get("PolyNftFactory")
+    const polyNftFactoryInstance = (await ethers.getContractAt(
+        "PolyNftFactory",
+        polyNftFactoryDeployments.address,
+    )) as PolyNftFactory
+
     const polyNftRegistryDeployments = await deployments.get("PolyNftRegistry")
     const polyNftRegistryInstance = (await ethers.getContractAt(
         "PolyNftRegistry",
         polyNftRegistryDeployments.address,
-    )) as PolyNftFactory
+    )) as PolyNftRegistry
 
-    const ipfsArray = [
-        "https://ipfs.io/ipfs/QmbPbq16xF4PsowtbSDKa4qcWFManZAnvYbfxhjMPgZ8Jw", // volley ball
-        "https://ipfs.io/ipfs/QmW9ukmor2y3SGjMp5VjA5QYvpsJ7n5bMo5etMizHLFCd7", // bubble tea 
-        "https://ipfs.io/ipfs/QmYhwAmRFCbsUFvySxMC8iT1qqj2kEzbi3ZQVUatuZxUPY", // coffee
-        "https://ipfs.io/ipfs/QmXkLzs3jo9APcUtzAWsG1gy7AmfN8r7L68uei3Ap1An2w", // diamond
-        "https://ipfs.io/ipfs/QmaH619MN8JBymMh1FtyGeLUQvBf3ZWUTy3pNsLCga3fyH", // head phone + bubble tea
-    ]
+    const projectName = "Cute Shark Family"
 
-    // await polyNftRegistryInstance.create(projectName, projectName, projectSymbol, baseURI, fusionImplementation)
+    const erc721Address = await polyNftFactoryInstance.projectMap(projectName)
+    const erc721Instance = (await ethers.getContractAt("IPolyNftErc721", erc721Address)) as IPolyNftErc721
+
+    await erc721Instance.approve(polyNftRegistryDeployments.address, 0)
+    await polyNftRegistryInstance.register({
+        polyNftErc721: "0x6c4c42fE8cc35f58eda92EA68B4a56fB28921C29",
+        tokenId: 0,
+        fusionCost: parseEther("0.01"),
+        description: `Register a new project to PolyNFT, tokenId: 0`,
+    })
+
+    await erc721Instance.approve(polyNftRegistryDeployments.address, 1)
+    await polyNftRegistryInstance.register({
+        polyNftErc721: "0x6c4c42fE8cc35f58eda92EA68B4a56fB28921C29",
+        tokenId: 1,
+        fusionCost: parseEther("0.01"),
+        description: `Register a new project to PolyNFT, tokenId: 1`,
+    })
 }
 
 main().catch(error => {
