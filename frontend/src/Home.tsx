f42843038c0cfca6435a27514daee39fb5d33dd1
Dylan Yang <dogi9527dogi9527@gmail.com>
2023-04-15 11:10:45 +0900

    frontend: import prettier and basic wagmi lib

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,9 @@
+import { Grid } from "@chakra-ui/react"
+
+export function Home() {
+    return (
+        <Grid bgColor={"black"} h="100vh" justifyContent={"center"} alignItems={"center"}>
+            NFT Polymerization
+        </Grid>
+    )
+}
