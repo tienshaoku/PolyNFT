c4f37cbd1c29a8cf4560c3f2ed2ac2aa02bf14f2
Philip Chen <lsps9150414@gmail.com>
2023-04-15 20:11:19 +0900

    add unbox animation

--- frontend/src/Merge.tsx
+++ frontend/src/Merge.tsx
@@ -1,4 +1,5 @@
-import { Container, Heading, Highlight, ColorModeScript, useColorMode } from "@chakra-ui/react"
+import { Container, Heading, Highlight } from "@chakra-ui/react"
+import { Header } from "Header"
 import { NFTItemProps } from "components/NFTItem"
 import NFTMergeManager from "components/NFTMergeManager"
 export function Merge() {
@@ -9,33 +10,33 @@ export function Merge() {
         console.log("hey")
     }
 
-    const { toggleColorMode } = useColorMode()
-
     return (
-        <Container>
-            <ColorModeScript />
-            <Heading my={8} textAlign={"center"}>
-                <Highlight query={"MERGE!"} styles={{ px: "2", py: "1", bg: "orange.100", rounded: "full" }}>
-                    Select up to 3 NFTs to MERGE!
-                </Highlight>
-            </Heading>
-            <NFTMergeManager
-                items={[
-                    { id: "0" },
-                    { id: "1" },
-                    { id: "2" },
-                    { id: "3" },
-                    { id: "4" },
-                    { id: "5" },
-                    { id: "6" },
-                    { id: "7" },
-                    { id: "8" },
-                    { id: "9" },
-                    { id: "10" },
-                    { id: "11" },
-                ]}
-                onUnboxSuccess={handleUnboxSuccess}
-            />
-        </Container>
+        <>
+            <Header />
+            <Container pt="120px">
+                <Heading mb={8} textAlign={"center"}>
+                    <Highlight query={"MERGE!"} styles={{ px: "2", py: "1", bg: "orange.100", rounded: "full" }}>
+                        Select up to 3 NFTs to MERGE!
+                    </Highlight>
+                </Heading>
+                <NFTMergeManager
+                    items={[
+                        { id: "0" },
+                        { id: "1" },
+                        { id: "2" },
+                        { id: "3" },
+                        { id: "4" },
+                        { id: "5" },
+                        { id: "6" },
+                        { id: "7" },
+                        { id: "8" },
+                        { id: "9" },
+                        { id: "10" },
+                        { id: "11" },
+                    ]}
+                    onUnboxSuccess={handleUnboxSuccess}
+                />
+            </Container>
+        </>
     )
 }
--- frontend/src/components/NFTMergeManager.tsx
+++ frontend/src/components/NFTMergeManager.tsx
@@ -1,7 +1,22 @@
-import { Box, Button, Flex, useToast } from "@chakra-ui/react"
+import {
+    Box,
+    Button,
+    Flex,
+    Highlight,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useToast,
+} from "@chakra-ui/react"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import NFTGrid from "./NFTGrid"
 import { NFTItemProps } from "./NFTItem"
+import { motion } from "framer-motion"
+import Box3D from "./Box3D"
 
 type Props = {
     items: NFTItemProps[]
@@ -9,10 +24,11 @@ type Props = {
     onUnboxSuccess?: (data: NFTItemProps) => void
 }
 
-const NFTMergeManager = ({ items, maxSelectionAmount = 3, onUnboxSuccess: onUnbox }: Props) => {
+const NFTMergeManager = ({ items, maxSelectionAmount = 3, onUnboxSuccess }: Props) => {
     const toast = useToast()
 
     const [selectedItems, setSelectedItems] = useState<string[]>([])
+    const isMergeValid = selectedItems.length > 0 && selectedItems.length <= maxSelectionAmount
 
     const handleSelect = (id: string) => {
         if (selectedItems.includes(id)) {
@@ -32,16 +48,26 @@ const NFTMergeManager = ({ items, maxSelectionAmount = 3, onUnboxSuccess: onUnbo
             setSelectedItems(nextSelectedItems)
         }
     }
+
+    const [isMerging, setIsMerging] = useState(false)
+
     const handlerMergeClick = () => {
         const sourceNFTItems = items.filter(({ id }) => selectedItems.includes(id))
         console.log(sourceNFTItems)
+        setIsMerging(true)
         // TODO: render shaking box animation
         // TODO: call image generator
         // TODO: receive image base64
         // TODO: call IPFS uploader backend, receive uri
         // TODO: call contract fuse(new img uri, description, data)
         // TODO: display unboxed new NFT!
-        onUnbox?.(items.find(({ id }) => id === selectedItems[0])!)
+        // onUnboxSuccess?.(items.find(({ id }) => id === selectedItems[0])!)
+        // setIsMerging(false)
+    }
+
+    const navigate = useNavigate()
+    const handleList = () => {
+        navigate("/list")
     }
 
     return (
@@ -52,10 +78,46 @@ const NFTMergeManager = ({ items, maxSelectionAmount = 3, onUnboxSuccess: onUnbo
                 marginBottom={4}
             />
             <Flex justifyContent={"center"}>
-                <Button size={"lg"} type="button" w="full" colorScheme="blue" onClick={handlerMergeClick}>
+                <Button size={"lg"} w="full" colorScheme="blue" onClick={handlerMergeClick} isDisabled={!isMergeValid}>
                     Merge!
                 </Button>
             </Flex>
+
+            <Modal size={"full"} isOpen={isMerging} onClose={() => null}>
+                <ModalOverlay />
+                <ModalContent backgroundColor="gray.900" color={"white"}>
+                    <ModalHeader textAlign={"center"} fontSize={"5xl"}>
+                        <Highlight query={"NEW NFT"} styles={{ px: "3", py: "1", bg: "yellow.300", rounded: "full" }}>
+                            {"HATCHING YOUR NEW NFT..."}
+                        </Highlight>
+                    </ModalHeader>
+                    <ModalBody>
+                        {isMerging && (
+                            <motion.div
+                                animate={{
+                                    opacity: [0.8, 1, 0.8, 1, 0.8, 1, 0.8, 1],
+                                    scale: [0.9, 1, 0.9, 1, 0.9, 1, 0.9, 1],
+                                    x: [0, -10, 5, -5, 10, -5, 5, 0],
+                                }}
+                                transition={{
+                                    duration: 0.5,
+                                    repeat: Infinity,
+                                    repeatDelay: 0.3,
+                                }}
+                            >
+                                <Box3D />
+                            </motion.div>
+                        )}
+                    </ModalBody>
+                    <ModalFooter>
+                        {
+                            <Button size={"lg"} w={"full"} colorScheme="blue" onClick={handleList}>
+                                List It Now
+                            </Button>
+                        }
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     )
 }
