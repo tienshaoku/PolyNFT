38d50c2fa619d38052c9272c48909d1a77e8723a
tienshaoku <tienshaoku@gmail.com>
2023-04-15 10:36:24 +0900

    # PolyNftErc721 updated.

--- contracts/PolyNftErc721.sol
+++ contracts/PolyNftErc721.sol
@@ -3,14 +3,14 @@ pragma solidity 0.8.18;
 import { ERC721PresetMinterPauserAutoId } from "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract PolyNftErc721 is ERC721PresetMinterPauserAutoId {
-    address private _registry;
-    address private _fustionImplentation;
-    mapping(uint256 => bytes) private _attributeMap;
-    mapping(uint256 => string) private _tokenUriMap;
+    address public registry;
+    address public fusionImplementation;
+    mapping(uint256 => bytes) public attributeMap;
+    mapping(uint256 => string) public tokenUrlMap;
 
     modifier onlyRegistry() {
         // PNE721_OR: PolyNftErc721 only regitry
-        require(msg.sender == _registry, "PNE721_OR");
+        require(msg.sender == registry, "PNE721_OR");
         _;
     }
 
@@ -18,33 +18,29 @@ contract PolyNftErc721 is ERC721PresetMinterPauserAutoId {
         string memory nameArg,
         string memory symbolArg,
         string memory baseTokenURIArg,
-        address fustionImplentationArg,
+        address fusionImplementationArg,
         address registryArg
     ) ERC721PresetMinterPauserAutoId(nameArg, symbolArg, baseTokenURIArg) {
-        _fustionImplentation = fustionImplentationArg;
-        _registry = registryArg;
+        fusionImplementation = fusionImplementationArg;
+        registry = registryArg;
     }
 
     // this function will be triggered after PolyNftRegistr.fustion()
     function setTokenURI(uint256 tokneIdArg, string calldata tokenURIArg) external onlyRegistry {
-        _tokenUriMap[tokneIdArg] = tokenURIArg;
+        tokenUrlMap[tokneIdArg] = tokenURIArg;
     }
 
     // this function will be triggered after PolyNftRegistr.fustion()
     function setTokenAttribute(uint256 tokneIdArg, bytes calldata attributeArg) external onlyRegistry {
-        _attributeMap[tokneIdArg] = attributeArg;
+        attributeMap[tokneIdArg] = attributeArg;
     }
 
     function getTokenAttribute(uint256 tokenId) external view returns (bytes memory) {
-        return _attributeMap[tokenId];
-    }
-
-    function getFustionImplentation() external view returns (address) {
-        return _fustionImplentation;
+        return attributeMap[tokenId];
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return bytes(_tokenUriMap[tokenId]).length > 0 ? _tokenUriMap[tokenId] : _baseURI();
+        return bytes(tokenUrlMap[tokenId]).length > 0 ? tokenUrlMap[tokenId] : _baseURI();
     }
 
 }
